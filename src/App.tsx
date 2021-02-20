075725786ac6f617ce6ea58b3c08b96d0a61d5f1
Chan <coin46@icloud.com>
2021-02-20 18:44:51 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>Debug</Text>
+    </View>
+  );
+};
+
+export default App;
